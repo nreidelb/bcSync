092f0e7f7b81171fd155ac8a56e0b30e0e7b35f5
Matt Boyajian <mboyajia@redhat.com>
2016-09-15 11:57:16 -0700

    Added stubs for custom WorkItemHandlers

---
+++ UpdateProject/src/main/java/com/partner/integration/conflictresolution/AutomatedResolutionHandler.java
@@ -0,0 +1,12 @@
+package com.partner.integration.conflictresolution;
+
+public class AutomatedResolutionHandler {
+
+	public AutomatedResolutionHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void automatedResolution() {
+		//TODO implement this
+	}
+}
---
+++ UpdateProject/src/main/java/com/partner/integration/git/PushPermissionHandler.java
@@ -0,0 +1,12 @@
+package com.partner.integration.git;
+
+public class PushPermissionHandler {
+
+	public PushPermissionHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void checkPushPermission() {
+		//TODO implement this
+	}
+}
